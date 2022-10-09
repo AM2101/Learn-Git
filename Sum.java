05eef8d16bdc4d601a86ddaddcbd116d61a44f82
Arsh Mittal <arshmittal451@gmail.com>
2022-10-09 13:35:31 +0530

    adding message in sum.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class Sum{
     public static void main(String arg[]){
         int a=6;
         int b=6;
-        System.out.println(a+b);
+        System.out.println("the sum is "+(a+b));
     }
 }
